70c195b61c96eb7c4c24682fe81f408b945d6953
Rohit Mathew <rohitjmathew@hotmail.com>
2016-11-06 00:32:54 +0530

    First push

--- app/src/main/java/com/example/rohitmathew/bucket_it/BucketMain.java
+++ app/src/main/java/com/example/rohitmathew/bucket_it/BucketMain.java
@@ -1,11 +1,16 @@
 package com.example.rohitmathew.bucket_it;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BucketMain extends AppCompatActivity {
 
@@ -21,6 +26,20 @@ public class BucketMain extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox1);
+        final TextView textView = (TextView) findViewById(R.id.Id);
+        if(checkBox.isChecked()){
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    textView.setCursorVisible(true);
+                    textView.setFocusableInTouchMode(true);
+                    textView.setInputType(InputType.TYPE_CLASS_TEXT);
+                    textView.requestFocus();
+                }
+            });
+        }
     }
 
 }
